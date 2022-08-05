44569dfd0d3dafdcedea189e4de929a011cecedf
sadbird1729 <68562748+sadbird1729@users.noreply.github.com>
2022-08-05 20:59:59 +0800

    Time: 91 ms (31.02%), Space: 14.5 MB (35.19%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sn = [0]*26
+        tn = [0]*26
+        for x in s:
+            sn[ord(x)-97] += 1
+        for x in t:
+            tn[ord(x)-97] += 1
+        for i in range(26):
+            if sn[i] != tn[i]:return False
+        return True
+        
